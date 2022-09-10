080edd81a8303411204d0e8f453be9bd69c1c308
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2022-09-10 15:29:19 +0200

    Add some type scetches

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+//pub struct Market {
+//    pub id: i64,
+//    pub name: String,
+//}
+//
+//pub struct Trade {
+//    pub market: Market,
+//    pub value: f32,
+//    pub count: f32,
+//}
+//
+//pub struct Bar {
+//    pub timestamp: i64,
+//    pub open: f32,
+//    pub high: f32,
+//    pub low: f32,
+//    pub close: f32,
+//}
+
 fn main() {
     println!("Hello, world!");
 }
