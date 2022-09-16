a4f0a30258ced06c0ed36f1980a83e6d9cde680c
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2022-09-16 22:26:07 +0200

    Add beginnings of Day concept
    I want something I can iterate on in parallel in order to run backtests
    on many days at the same time for speed. I want to test this concept
    early as the main way this makes use of rust concurrency while keeping
    all most other code single threaded and simple to reason about.

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,24 @@ pub struct Bar {
 }
 
 #[derive(Debug)]
+pub struct Day {
+    pub date : Date<Local>,
+    //pub open_time : DateTime<Local>,
+    //pub close_time : DateTime<Local>,
+
+    //pub open : f32,
+    //pub high : f32,
+    //pub low : f32,
+    //pub close : f32,
+    //pub volume : u64,
+}
+
+#[derive(Debug)]
 pub struct Chart {
     pub symbol : String,
     pub timeframe : i32,
     pub bars : Vec<Bar>,
+    pub days : Vec<Day>,
 }
 
 // This is the data strategies will have to make decisions
@@ -53,10 +67,15 @@ fn main() {
     // - figure out how simulated trading would work
     // - run more than one stock at the same time
 
-    dbg!(chart);
+    dbg!(&chart.days[305]);
 }
 
 fn load_chart(path : &str) -> Chart {
+    let re = Regex::new(r"data/(.+)/(.+)\.json").unwrap();
+    let cap = re.captures(path).unwrap();
+    let timeframe = &cap[1];
+    let symbol = &cap[2];
+
     let data = fs::read_to_string(path)
         .expect("Unable to read file at {path}.");
 
@@ -79,14 +98,33 @@ fn load_chart(path : &str) -> Chart {
         })
     };
 
-    let re = Regex::new(r"data/(.+)/(.+)\.json").unwrap();
-    let cap = re.captures(path).unwrap();
-    let timeframe = &cap[1];
-    let symbol = &cap[2];
+    let mut current_date = bars[0].time.date();
+    let mut days : Vec<Day> = Vec::new();
+
+    // TODO: This does not include a day for the last day since there
+    //       will never be a bar day newer than the current date.
+    //
+    //       I think I want it to add a day once it reaches the end
+    //       of the current day in order to be able to collect
+    //       open/close, etc.
+    //
+    //       But maybe just a group would work better?
+    for bar in &bars {
+        let bar_date = bar.time.date();
+
+        if bar_date > current_date {
+            days.push(Day {
+                date: current_date
+            });
+
+            current_date = bar.time.date();
+        }
+    }
 
     Chart {
         symbol: symbol.to_string(),
         timeframe: timeframe.parse::<i32>().unwrap(),
-        bars: bars
+        bars: bars,
+        days: days
     }
 }
