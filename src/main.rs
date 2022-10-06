442e9086067f10f5df1c3e25f0b4a2b220134cfc
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2022-10-06 16:03:34 +0200

    Add result collection from parallel simulation

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,23 @@ use crate::types::*;
 fn main() {
     let chart = load::load_chart("AZA", 15, "data/local/15/AZA.json");
 
-    let mut results : Vec<DayResult> = Vec::new();
+    let results : Vec<DayResult> = chart.days.par_iter()
+        .map(|day| { dbg!("map"); simulator::simulate_day(&chart, day) })
+        .collect();
 
-    chart.days.par_iter().map(|day| {
-        simulator::simulate_day(&chart, day);
-    });
-
-    // wip get data back
-    //.reduce(|| (DayResult { percent: 0 }), |a, b| {
-    //})
-
-    //dbg!(results);
+    dbg!(chart.days.len());
+    dbg!(results.len());
+    let mut fixed_change = 100f32;
+    // exponential_change
+    for r in results {
+        // this is just a placeholder calculation
+        fixed_change = fixed_change + r.percent;
+    }
+    dbg!(fixed_change);
 
     // next:
-    // - cleanup chart loading code, put that into a separate file
-    // - separate into days and run them in parallel
+    // x cleanup chart loading code, put that into a separate file
+    // x separate into days and run them in parallel
     // - figure out how simulated trading would work
     // - run more than one stock at the same time
 
