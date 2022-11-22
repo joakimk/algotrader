6280f58ba109a5a5eaf52d255ac388a9b721bff4
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2022-11-22 22:04:40 +0100

    Implement position_percentage_of_current_account_size

--- src/simulator.rs
+++ src/simulator.rs
@@ -10,12 +10,13 @@ pub fn simulate_day(settings: &Settings, chart: &Chart, day: &Day, account_size_
 
         // WIP: Overslimplified: Buy open, sell close of day.
         if trades.len() == 0 && previous_day.close > previous_day.open {
+            let max_position_size = settings.position_minimal_amount.max((settings.position_percentage_of_current_account_size / 100.0) * account_size_at_open);
             let buy_time = day.open_time;
             let buy_price = day.open;
 
             // todo: use position_percentage_of_current_account_size as well
             // make this a test ^
-            let buy_count = (settings.position_minimal_amount / buy_price) as u32;
+            let buy_count = (max_position_size / buy_price) as u32;
             let buy_total = (buy_count as f32) * buy_price;
 
             let sell_time = day.close_time;
@@ -28,7 +29,7 @@ pub fn simulate_day(settings: &Settings, chart: &Chart, day: &Day, account_size_
                 buy_price: buy_price,
                 buy_count: buy_count,
                 rounded_position_amount: ((buy_count as f32) * buy_price) as u32,
-                rounded_position_unused_amount: (settings.position_minimal_amount - buy_total) as u32,
+                rounded_position_unused_amount: (max_position_size - buy_total) as u32,
                 sell_price: sell_price,
                 fee_amount: settings.fee_per_transaction * 2.0,
             });
@@ -48,7 +49,6 @@ pub fn simulate_day(settings: &Settings, chart: &Chart, day: &Day, account_size_
         let unused_amount = account_amount - position_amount;
         account_amount = unused_amount + position_amount * diff;
         fee_amount += trade.fee_amount;
-        dbg!(trade);
     }
 
     let account_size_at_close = account_amount;
@@ -97,17 +97,10 @@ mod simulate_day {
         let account_size_before_day = settings.account_initial_size;
         let day_result = simulate_day(&settings, &chart, &day, account_size_before_day);
 
-        dbg!(&day_result);
+        //dbg!(&day_result);
 
         assert_eq!(day_result.time, Local.ymd(2022, 8, 17).and_hms(9, 0, 0));
         assert_eq!(day_result.fee_amount, 2.0);
-
-        // The diff between buy and sell is 3.35%, but we do not use
-        // the entire amount so in total the account change is 3.22%.
-        // ruby: (1 - (((198.5 * 5) * (192.05/198.5) + (1000 - (198.5 * 5))) / 1000.0)).round(5) * 100
-        //assert_eq!(day_result.percent, -(3.225));
-        // todo: reimplement this for open and close account size
-        //assert_eq!(false, true);
 
         assert_eq!(day_result.account_size_at_open, settings.account_initial_size);
 
@@ -116,6 +109,31 @@ mod simulate_day {
         // 1191 decreases by 3.25% to 1152.3
         // What remains is 1152.3 which if you add the unused 109 you get 1261.3.
         assert_eq!(day_result.account_size_at_close, 2961.3);
+    }
+
+    #[test]
+    fn test_can_use_larger_positions_as_the_account_grows() {
+        let settings = Settings {
+            account_initial_size: 3000.0,
+            position_minimal_amount: 1300.0,
+            position_percentage_of_current_account_size: 45.0,
+            fee_per_transaction: 1.0,
+        };
+
+        let chart = load_about_a_month_of_stock_data();
+        let day = &chart.days[3];
+
+        let account_size_before_day = 3500.0;
+        let day_result = simulate_day(&settings, &chart, &day, account_size_before_day);
+
+        dbg!(&day_result);
+
+        assert_eq!(day_result.trades.len(), 1);
+        let trade = &day_result.trades[0];
+        let position_size_used : f32 = (trade.rounded_position_amount + trade.rounded_position_unused_amount) as f32;
+
+        // We expect it to be 45% of 3500.0 (which should be 1575 but we use a rounded amount).
+        assert_eq!(position_size_used, 1574.0);
     }
 
     fn load_about_a_month_of_stock_data() -> Chart {
