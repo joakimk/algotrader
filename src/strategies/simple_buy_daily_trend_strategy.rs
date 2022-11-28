ead2f6c07109f6c6f00265f9a16418d77d809e71
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2022-11-28 22:27:34 +0100

    Improve strategy slightly
    Next up is probably implementing moving average have the simple strategy respect market trend.

--- src/strategies/simple_buy_daily_trend_strategy.rs
+++ src/strategies/simple_buy_daily_trend_strategy.rs
@@ -2,7 +2,7 @@ use crate::types::*;
 use crate::strategy_helpers::*;
 
 pub fn trade(_chart : &Chart, today: &PartialDay, previous_day: &Day, bar : &Bar, _active_trade : &Option<ActiveTrade>) -> Action {
-    if market_opened_minutes_ago(today, bar, 30) && was_bullish(previous_day) {
+    if market_opened_minutes_ago(today, bar, 30) && bar.close < today.open && was_bullish(previous_day) {
         Action::EnterLong
     } else if market_closes_in_minutes(today, bar, 30) {
         Action::Exit
