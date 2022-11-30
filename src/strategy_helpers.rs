124eef2bee58740233e7f6f8f881e2317863347d
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2022-11-30 12:25:46 +0100

    Add moving average helpers
    No clear win in the current strategy but they appear to work correctly (have not verified manually).

--- src/strategy_helpers.rs
+++ src/strategy_helpers.rs
@@ -11,3 +11,34 @@ pub fn market_closes_in_minutes(today : &PartialDay, bar : &Bar, minutes : i64) 
 
 pub fn add_min(time : DateTime<Local>, minutes : i64) -> DateTime<Local> { time + Duration::minutes(minutes) }
 pub fn sub_min(time : DateTime<Local>, minutes : i64) -> DateTime<Local> { time - Duration::minutes(minutes) }
+
+#[allow(dead_code)]
+pub fn close_above_moving_average(bars : &Vec<Bar>, bar : &Bar, length : usize) -> bool {
+    if bars.len() > length {
+        bar.close > moving_average(bars, length)
+    } else {
+        false
+    }
+}
+
+#[allow(dead_code)]
+pub fn close_below_moving_average(bars : &Vec<Bar>, bar : &Bar, length : usize) -> bool {
+    if bars.len() > length {
+        bar.close < moving_average(bars, length)
+    } else {
+        false
+    }
+}
+
+#[allow(dead_code)]
+pub fn moving_average(bars : &Vec<Bar>, length : usize) -> f32{
+    let total_length = bars.len();
+    let bars_within_moving_average_period = &bars[(total_length - length - 1)..(total_length - 1)];
+
+    let mut sum = 0.0;
+    for bar in bars_within_moving_average_period {
+        sum += bar.close;
+    }
+
+    sum / (length as f32)
+}
