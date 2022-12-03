965758b3fccf90f3eaedad9721eff2da90986cbe
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2022-12-03 09:52:56 +0100

    Cleanup

--- src/strategies/simple_buy_daily_trend_strategy.rs
+++ src/strategies/simple_buy_daily_trend_strategy.rs
@@ -2,10 +2,10 @@ use crate::types::*;
 use crate::strategy_helpers::*;
 
 pub fn trade(_symbol : &String, _bars : &Vec<Bar>, today: &PartialDay, previous_day: &Day, bar : &Bar, _active_trade : &Option<ActiveTrade>) -> Action {
+    if market_closes_in_minutes(today, bar, 30) { return Action::Exit }
+
     if market_opened_minutes_ago(today, bar, 30) && bar.close < today.open && was_bullish(previous_day) {
         Action::EnterLong
-    } else if market_closes_in_minutes(today, bar, 30) {
-        Action::Exit
     } else {
         Action::None
     }
